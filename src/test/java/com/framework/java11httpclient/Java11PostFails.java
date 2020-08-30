1b628d9634e98fd27a55f62e20ca0f209412bc81
Kristina Markova <kristinamarkovaa@gmail.com>
2020-08-31 01:51:49 +0200

    Creating few more tests and trying out RestAssured library

--- src/test/java/com/framework/java11httpclient/Java11PostFails.java
+++ src/test/java/com/framework/java11httpclient/Java11PostFails.java
@@ -1,5 +1,7 @@
 package com.framework.java11httpclient;
 
+import com.testframework.entities.FailedAuthentication;
+import com.testframework.handlers.JsonBodyHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +11,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-import static java.net.http.HttpClient.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static java.net.http.HttpClient.newBuilder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Java11PostFails {
 
@@ -34,5 +36,22 @@ public class Java11PostFails {
         // Assert
         assertEquals(401, actualCode);
 
+    }
+
+    @Test
+    void postFailsContainsMessage() throws IOException, InterruptedException {
+        // Arrange - create client
+        HttpClient httpClient = newBuilder().build();
+
+        // Arrange - create request
+        HttpRequest get = HttpRequest.newBuilder(URI.create(BASE_URL + "user/repos"))
+                .POST(HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        HttpResponse<FailedAuthentication> response = httpClient.send(get, JsonBodyHandler.jsonBodyHandler(FailedAuthentication.class));
+
+        String actualMessage = response.body().getMessage();
+
+        Assertions.assertEquals("Requires authentication", actualMessage);
     }
 }
---
+++ src/test/java/com/framework/restassured/BasicRestAssuredApiTest.java
@@ -0,0 +1,34 @@
+package com.framework.restassured;
+
+import org.junit.jupiter.api.Test;
+
+
+public class BasicRestAssuredApiTest {
+
+    @Test
+    public void getStatusCodeIs200() {
+
+//        Restassured.get("https://api.github.com/")
+//                .then()
+//                .statusCode(200);
+
+    }
+
+//    @Test
+//    public void headersContainCorrectValues() {
+//        RestAssured.get("https://api.github.com/")
+//                .then()
+//                .assertThat()
+//                .header("content-type", "application/json; charset=utf-8")
+//                .header("X-Ratelimit", "60");
+//    }
+
+//    @Test
+//    public void bodyContainsCorrectValues() {
+//        RestAssured.get("https://api.github.com/users/kristinamarkova")
+//                .then()
+//                .assertThat()
+//                .body("login", equalTo("kristinamarkova"))
+//                .body("type", equalTo("User"));
+//    }
+}
